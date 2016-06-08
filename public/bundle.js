c5658d43b701f6c50de39f948e9c2fc35b9f55c0
Sam Gruse <sgruse89@gmail.com>
2016-06-08 10:25:37 -0700

    changed fonts and fixed buttons

--- public/bundle.js
+++ public/bundle.js
@@ -31350,8 +31350,8 @@
 
 	
 
-	exports.baseUrl = 'https://overbrook-server.herokuapp.com'
-	//exports.baseUrl = 'http://localhost:3000'
+	// exports.baseUrl = 'https://overbrook-server.herokuapp.com'
+	exports.baseUrl = 'http://localhost:3000'
 
 
 /***/ },
