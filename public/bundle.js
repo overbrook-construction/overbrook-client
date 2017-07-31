2e1879f360322134e33aeb8e0be9d3f7b42fc2dc
DAVID HEYDUCK <heyduckd@gmail.com>
2017-07-31 10:13:49 -0700

    changed the top available to 3306 and made it so we can set 2nd spot too

--- public/bundle.js
+++ public/bundle.js
@@ -31279,7 +31279,9 @@
 	  var adminRoute = constants.baseUrl + '/addHomes';
 
 	  var obj = {};
+	  var obj2 = {};
 
+	  obj2.allHomeData2;
 	  obj.allHomeData;
 
 	  obj.sayName = function() {
@@ -31288,21 +31290,31 @@
 	  obj.getData = function(cb) {
 	    $http.get(adminRoute)
 	    .then(function successCallback(response) {
-	      obj.allHomeData = response.data;
+
+	      obj2.allHomeData2 = response.data;
+
+	      for (var i = 0; i < obj2.allHomeData2.length; i++) {
+	        if (obj2.allHomeData2[i]._id === "58be64f38359750400f02164") {
+	          var houseToMove = obj2.allHomeData2.splice(i, 1)[0]
+	          obj2.allHomeData2.unshift(houseToMove)
+	          obj.allHomeData = obj2.allHomeData2;
+	        }
+	      }
 
 	      for (var i = 0; i < obj.allHomeData.length; i++) {
 	        if (obj.allHomeData[i]._id === "58be58bbd914590400de7e23") {
-	          var houseToMove = obj.allHomeData.splice(i, 1)[0]
-	          obj.allHomeData.unshift(houseToMove)
+	          var houseToMove2 = obj.allHomeData.splice(i, 1)[0]
+	          obj.allHomeData.unshift(houseToMove2)
 	        }
 	      }
 
-	      for (var i = 0; i < obj.allHomeData.length; i++) {
-	        if (obj.allHomeData[i]._id === "58be64f38359750400f02164") {
-	          var houseToMove = obj.allHomeData.splice(i, 2)[0]
-	          obj.allHomeData.unshift(houseToMove)
-	        }
-	      }
+
+	      // for (var i = 0; i < obj.allHomeData.length; i++) {
+	      //   if (obj.allHomeData[i]._id === "58be64f38359750400f02164") {
+	      //     var houseToMove = obj.allHomeData.splice(i, 2)[0]
+	      //     obj.allHomeData.unshift(houseToMove)
+	      //   }58be58bbd914590400de7e23
+	      // }
 
 	      $window.localStorage.setItem('allHomeData', JSON.stringify(obj.allHomeData));
 	      cb();
